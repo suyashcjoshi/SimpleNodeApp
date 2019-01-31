7e27a9dedf5edcdd4751d577e7f03650eb0ce0d1
Suyash Joshi <suyashjoshi@users.noreply.github.com>
2019-01-31 15:20:10 -0800

    adding the two files
    Simple server and manifest file for this project

---
+++ MyServer.js
@@ -0,0 +1,11 @@
+var http = require("http");
+// Read Enviornment Parameters
+var port = Number(process.env.PORT || 8080);
+var greeting = (process.env.GREETING || "Hello World from Oracle Cloud!");
+// Create a simple HTTP Server
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end(greeting + "\n");
+});
+// Server is now listening on the port configured above
+server.listen(port);
